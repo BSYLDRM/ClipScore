d8920abeea14bdca61ac00042b46e44a37f5c299
buse yıldırım <21060670@stu.omu.edu.tr>
2026-04-22 14:36:03 +0300

    Update MainActivity with LaunchScreen and add IDE configuration files

--- app/src/main/java/com/example/clipscore/MainActivity.kt
+++ app/src/main/java/com/example/clipscore/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.clipscore.ui.theme.ClipScoreTheme
 
@@ -19,29 +22,32 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ClipScoreTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                LaunchScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun LaunchScreen(modifier: Modifier = Modifier) {
+    Box(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .background(Color.Black),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "iyiki varsınız <3",
+            color = Color.White,
+            fontSize = 24.sp
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun LaunchScreenPreview() {
     ClipScoreTheme {
-        Greeting("Android")
+        LaunchScreen()
     }
 }
